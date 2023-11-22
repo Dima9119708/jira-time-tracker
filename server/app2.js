47084268beade6f4343ec51d0dde824da183d447
Dima9119708 <tipanswer@gmail.com>
2023-11-22 15:03:56 +0200

    Fix

--- server/app2.js
+++ server/app2.js
@@ -1,10 +1,15 @@
 import express from 'express'
 import cors from 'cors'
+import cookieParser from 'cookie-parser'
 
 const app = express()
 const port = 3000
 
 app.use(cors({ origin: true, credentials: true }))
+
+app.use(cookieParser())
+
+app.use(express.json())
 
 app.get('/', (req, res) => {
     res.send(`
