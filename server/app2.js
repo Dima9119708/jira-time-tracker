8d1bbd2f149cf83ecdedb0d67cbb3d65a1ac34bb
Dima9119708 <tipanswer@gmail.com>
2023-11-22 11:35:57 +0200

    remove .idea

--- server/app2.js
+++ server/app2.js
@@ -1,10 +1,7 @@
 import express from "express";
-import cors from "cors";
 
 const app = express();
 const port = 3000;
-
-app.use(cors({ origin: true, credentials: true }))
 
 app.get('/', (req, res) => {
     res.send('Hello');
