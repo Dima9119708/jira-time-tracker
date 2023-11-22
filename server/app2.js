2c860295e61cbae9a264a428961b254265282480
Dima9119708 <tipanswer@gmail.com>
2023-11-22 12:33:33 +0200

    remove .idea

--- server/app2.js
+++ server/app2.js
@@ -1,10 +1,13 @@
 import express from "express";
 import cors from 'cors'
+import cookieParser from 'cookie-parser'
 
 const app = express();
 const port = 3000;
 
 app.use(cors({ origin: true, credentials: true }))
+
+app.use(cookieParser())
 
 app.get('/test', (req, res) => {
     res.send('Hello');
