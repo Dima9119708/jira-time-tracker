391e097adb6913cc96e4960d400c91ed860a6679
Dima9119708 <tipanswer@gmail.com>
2023-12-14 13:27:22 +0200

    New features

--- client/src/react-app/app/router/router.tsx
+++ client/src/react-app/app/router/router.tsx
@@ -4,11 +4,15 @@ import { queryClient } from '../QueryClientProvide/QueryClientProvide'
 import { loaderAuth } from '../../features/authByEmailAndToken'
 import LayoutRoot from '../LayoutRoot/LayoutRoot'
 
-const createRouter = (routes: RouteObject[]) => (__BUILD_ENV__ === 'browser' ? createBrowserRouter(routes) : createHashRouter(routes))
+const createRouter = (routes: RouteObject[]) => {
+    return __BUILD_ENV__ === 'browser'
+        ? createBrowserRouter(routes, { basename: __BASE_APP_ROUTE__ })
+        : createHashRouter(routes, { basename: __BASE_APP_ROUTE__ })
+}
 
 export const router = createRouter([
     {
-        path: __BASE_APP_ROUTE__,
+        path: '',
         Component: LayoutRoot,
         loader: loaderAuth(queryClient),
         shouldRevalidate: (args) => false,
@@ -48,7 +52,7 @@ export const router = createRouter([
         ],
     },
     {
-        path: `${__BASE_APP_ROUTE__}/auth`,
+        path: `auth`,
         Component: AuthPage,
     },
     {
