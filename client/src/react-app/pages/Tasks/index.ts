efe028355117d684be113acc9d7e258031ee2ecd
Dima9119708 <tipanswer@gmail.com>
2023-12-26 22:49:28 +0200

    REFACTOR

--- client/src/react-app/pages/Tasks/index.ts
+++ client/src/react-app/pages/Tasks/index.ts
@@ -1,5 +1,11 @@
 import { queryGetFilter } from './model/queryOptions'
 import TasksPage from './ui/TasksPage'
+<<<<<<< HEAD
 import { loaderTasks } from './loader/loader'
 
 export { queryGetFilter, TasksPage, loaderTasks }
+=======
+import { loaderFilters } from './loader/loader'
+
+export { queryGetFilter, TasksPage, loaderFilters }
+>>>>>>> 9eb710d (REFACTOR)
